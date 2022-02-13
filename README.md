28e559c6daa435c21217b1387d2f888c1f9a7029
BrunoBerger <bruno.berger@stud.hshl.de>
2022-02-13 17:32:53 +0100

    simple get reqwest +  .env file setup for tokens

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Twitter-Watch
 Playing around with rust and web-stuff
+
+### Setup
+1. Clone and create a ```.env``` file in the project root with this:
+```json
+BEARER_TOKEN="your_twitter_bearer_token_here"
+```
+2. Run ``` cargo run ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> Result<(), reqwest::Error> {
+    let token = dotenv::var("BEARER_TOKEN").unwrap();
+    dotenv::dotenv().ok();
+    println!("{}", token);
+
+    let body = reqwest::blocking::get("http://example.com/")?.text()?;
+    println!("body = {:?}", body);
+    Ok(())
 }
