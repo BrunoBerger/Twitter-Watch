3f76e9e69c497d09c735cdc641d8a35344483603
BrunoBerger <bruno.berger@stud.hshl.de>
2022-02-15 23:41:14 +0100

    more detailed GET

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Playing around with rust and web-stuff
 
 ### Setup
 1. Clone and create a ```.env``` file in the project root with this:
-```json
+```.env
 BEARER_TOKEN="your_twitter_bearer_token_here"
 ```
 2. Run ``` cargo run ```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 
-fn main() -> Result<(), reqwest::Error> {
-    let token = dotenv::var("BEARER_TOKEN").unwrap();
-    dotenv::dotenv().ok();
-    println!("{}", token);
+fn blocking_get(url: &str, token: &str) -> Result<reqwest::blocking::Response, reqwest::Error> {
+    let client = reqwest::blocking::Client::new();
+    let mut map = std::collections::HashMap::new();
+    map.insert("lang", "rust");
+    let resp = client.get(url)
+        .bearer_auth(token)
+        .json(&map)
+        .header(reqwest::header::USER_AGENT, "what dis")
+        .body("I am Boddy!")
+        .send()?;
+    Ok(resp)
+}
 
-    let body = reqwest::blocking::get("http://example.com/")?.text()?;
-    println!("body = {:?}", body);
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let token = dotenv::var("BEARER_TOKEN").unwrap();
+    let url = dotenv::var("URL").unwrap();
+    dotenv::dotenv().ok();
+    println!("Sending req to {}", url);
+
+    let resp = blocking_get(&url, &token)?;
+    println!("Status: {}", resp.status());
+    println!("{:#?}", resp);
+    println!("{:?}", resp.text());
     Ok(())
 }
