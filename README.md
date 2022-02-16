38de5453c9755594a5622a76b5834e702956c937
BrunoBerger <bruno.berger@stud.hshl.de>
2022-02-16 23:53:42 +0100

    minimal working get reqest to twitter

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Playing around with rust and web-stuff
 ### Setup
 1. Clone and create a ```.env``` file in the project root with this:
 ```.env
-BEARER_TOKEN="your_twitter_bearer_token_here"
+TW_BEARER_TOKEN="your_twitter_bearer_token_here"
+TW_USER="id_of_user_you_want_to_watch"
+TW_WORD="word_to_search_for"
 ```
 2. Run ``` cargo run ```
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 
 fn blocking_get(url: &str, token: &str) -> Result<reqwest::blocking::Response, reqwest::Error> {
     let client = reqwest::blocking::Client::new();
-    let mut map = std::collections::HashMap::new();
-    map.insert("lang", "rust");
     let resp = client.get(url)
         .bearer_auth(token)
-        .json(&map)
-        .header(reqwest::header::USER_AGENT, "what dis")
-        .body("I am Boddy!")
         .send()?;
     Ok(resp)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let token = dotenv::var("BEARER_TOKEN").unwrap();
-    let url = dotenv::var("URL").unwrap();
+    let token = dotenv::var("TW_BEARER_TOKEN").unwrap();
+    let user = dotenv::var("TW_USER").unwrap();
+    let word = dotenv::var("TW_WORD").unwrap();
     dotenv::dotenv().ok();
+    let url = format!("https://api.twitter.com/2/users/{user}/tweets?max_results=5");
     println!("Sending req to {}", url);
 
     let resp = blocking_get(&url, &token)?;
     println!("Status: {}", resp.status());
-    println!("{:#?}", resp);
-    println!("{:?}", resp.text());
+
+    let message = resp.text()?;
+    let split = message.split("{");
+    for s in split {
+        println!("{}", s)
+    }
+
+    println!("contains '{word}': {}", message.to_lowercase().contains(&word));
     Ok(())
 }
